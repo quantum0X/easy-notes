41eb68113daf615aefaa4b493580f25d431c7426
quantum0X <thevishal2108@gmail.com>
2023-07-31 00:51:29 +0530

    new route created for signup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { router } from './router/PublicRoute'
-import NewUser from './Pages/NewUser';
 function App() {
   return (
     <div className="App">
       <RouterProvider router={router} />
-      <NewUser />
     </div>
   );
 }
--- src/router/PublicRoute.js
+++ src/router/PublicRoute.js
@@ -1,10 +1,12 @@
 import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import Dashboard from "../components/Dashboard";
+import NewUser from "../Pages/NewUser";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route>
             <Route path="/" element={<Dashboard />} />
+            <Route path="/signup" element={<NewUser />} />
         </Route>
     )
 )
