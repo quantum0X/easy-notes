873ae2edee11f8d1fd14e5a0fbb18f110fbd6c3b
quantum0X <thevishal2108@gmail.com>
2023-07-31 02:02:38 +0530

    redirect completed -> <NewUser/>

--- src/Pages/NewUser.jsx
+++ src/Pages/NewUser.jsx
@@ -1,5 +1,5 @@
 import { Button, Form, Input, Space } from "antd";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
@@ -27,9 +27,14 @@ const NewUser = () => {
       await signUpHandle(mail, password);
       navigate("/");
     } catch (err) {
-      console.error(err.code);
+      console.error(err);
     }
   };
+
+  useEffect(() => {
+    if (currentUser) return navigate("/");
+  });
+
   return (
     <div
       style={{
@@ -42,45 +47,49 @@ const NewUser = () => {
         backgroundSize: "cover",
       }}
     >
-      <Space
-        style={{
-          height: "50%",
-          width: "50%",
-          backgroundImage: `url(${process.env.PUBLIC_URL + "/img/img-1.png"})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          backdropFilter: "blur(8px)",
-        }}
-      >
-        {/* <Image src={BackImage} alt="img" /> */}
-        <Form>
-          <Form.Item>
-            <Input
-              type="text"
-              placeholder="Email Address"
-              value={mail}
-              onChange={(e) => setMail(e.target.value)}
-            />
-          </Form.Item>
-          <Form.Item>
-            <Input
-              type="password"
-              placeholder="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Item>
-          <Form.Item>
-            <Input
-              type="password"
-              placeholder="confirm password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-          </Form.Item>
-          <Button onClick={submitHandle}>Log in</Button>
-        </Form>
-      </Space>
+      {currentUser && (
+        <Space
+          style={{
+            height: "50%",
+            width: "50%",
+            backgroundImage: `url(${
+              process.env.PUBLIC_URL + "/img/img-1.png"
+            })`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backdropFilter: "blur(8px)",
+          }}
+        >
+          {/* <Image src={BackImage} alt="img" /> */}
+          <Form>
+            <Form.Item>
+              <Input
+                type="text"
+                placeholder="Email Address"
+                value={mail}
+                onChange={(e) => setMail(e.target.value)}
+              />
+            </Form.Item>
+            <Form.Item>
+              <Input
+                type="password"
+                placeholder="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Item>
+            <Form.Item>
+              <Input
+                type="password"
+                placeholder="confirm password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </Form.Item>
+            <Button onClick={submitHandle}>Log in</Button>
+          </Form>
+        </Space>
+      )}
     </div>
   );
 };
