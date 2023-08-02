68023bfbd2bf968b11eed18e1099c1fa0c4b44ce
quantum0X <thevishal2108@gmail.com>
2023-08-02 23:54:56 +0530

    profile page created

---
+++ src/Pages/Profile.jsx
@@ -0,0 +1,16 @@
+import { Image, Input, Space } from "antd";
+import React from "react";
+
+const Profile = () => {
+  return (
+    <Space style={{ width: "100vw", height: "90vh" }}>
+      <Space>
+        <Space>
+          <Image />
+        </Space>
+      </Space>
+    </Space>
+  );
+};
+
+export default Profile;
